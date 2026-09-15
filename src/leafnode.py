fe32de0c13ef2e2c10e719d75dfbdea371248b8a
chancecarr <chancecarr1@gmail.com>
2026-09-15 13:42:56 -0600

    Add LeafNode

---
+++ src/leafnode.py
@@ -0,0 +1,16 @@
+from htmlnode import HTMLNode
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str | None, value: str, props: dict[str, str] | None = None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self) -> str:
+        if self.value is None: raise ValueError
+        if self.tag is None: return str(self.value)
+        if self.props is None or self.props == {}:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        else:
+            return f"<{self.tag} {super().props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
---
+++ src/test_leafnode.py
@@ -0,0 +1,15 @@
+import unittest
+from leafnode import LeafNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_to_html_basic(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_with_props(self):
+            node = LeafNode("a", "Link", {"href": "https://www.google.com", "target": "_blank"})
+            self.assertEqual(node.to_html(), '<a href="https://www.google.com" target="_blank">Link</a>')
+
+    def test_repr(self):
+        node = LeafNode("a", "Link", {"href": "https://www.google.com", "target": "_blank"})
+        self.assertEqual(str(node), "LeafNode(a, Link, {'href': 'https://www.google.com', 'target': '_blank'})")
