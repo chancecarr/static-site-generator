0e9fd53e58c520d25e8968cfb23ca6ecbdc14e5b
chancecarr <chancecarr1@gmail.com>
2026-09-23 16:36:53 -0600

    Rewrite docs page

--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@ from generatecontent import copy_from_source_to_destination, generate_pages_recu
 import sys
 
 def main():
-    basepath = "/" if len(sys.argv) < 2 else sys.argv[1]
-    copy_from_source_to_destination("./static", "./docs")
-    generate_pages_recursive("./content", "./template.html", "./docs", basepath)
+    target_dir = "." if len(sys.argv) < 2 else sys.argv[1]
+    base_url_path = "/" if len(sys.argv) < 3 else sys.argv[2]
+    copy_from_source_to_destination(f"{target_dir}/static", f"{target_dir}/docs")
+    generate_pages_recursive(f"{target_dir}/content", "./template.html", f"{target_dir}/docs", base_url_path)
 
 if __name__ == "__main__":
     main()
